13a4fb1bfd56205ef01ec03257e5eb42f2ac45d8
Serg Ch <chivirdos@gmail.com>
2021-03-07 00:35:16 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!arguments.length || array.length === 0) return 0;
+
+  let minValue = array[0]
+  array.forEach(element => {
+    if (element < minValue) {
+      minValue = element;
+    }
+  });
+
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!arguments.length || array.length === 0) return 0;
+
+  let maxValue = array[0];
+  array.forEach(element => {
+    if (element > maxValue) {
+      maxValue = element;
+    }
+  });
+
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!arguments.length || array.length === 0) return 0;
+
+  let avg = 0;
+  array.forEach(element => {
+    avg += element;
+  });
+
+  return avg / array.length;
 }
